d3ad06552e113b39e8f3db2ca2635e7a9667ef68
Yasmin Tavares <yamin-tavares-f@hotmail.com>
2023-12-06 18:48:40 -0300

    Fazer um programa que leia três valores com ponto flutuante de dupla precisão: A, B e C. Em seguida, calcule e mostre: a) a área do triângulo retângulo que tem A por base e C por altura. b) a área do círculo de raio C. (pi = 3.14159) c) a área do trapézio que tem A e B por bases e C por altura. d) a área do quadrado que tem lado B. e) a área do retângulo que tem lados A e B.

---
+++ Ex6/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro número: ");
+            float A= float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            float B = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o terceito número: ");
+            float C = float.Parse(Console.ReadLine());
+
+            // Área do triangulo 
+
+            float atrig = (A * C) / 2;
+            Console.WriteLine("Área do triangulo é: "+ atrig);
+
+            // Área do circulo
+
+            double acirc = 3.14159 * (C * C);
+            Console.WriteLine("Área do circulo é: "+ acirc);
+
+            // Área do trapézio 
+
+            float atrap = (A + B)* C/2;
+            Console.WriteLine("Área do trapézio é: " + atrap);
+
+
+            // Área do quadrado 
+            float aquad = B * B;
+            Console.WriteLine("Área do quadrado é: " + aquad);
+
+
+            // Área do retangulo
+            float aret = A * B;
+            Console.WriteLine("Área do retangulo é: " + aret);
+        }
+    }
+}
